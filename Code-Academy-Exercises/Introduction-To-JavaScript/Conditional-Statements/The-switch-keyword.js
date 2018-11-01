59fc2e382b0a8010176c6c014e89d51ee3ff6c90
Samuel Slike <samcslike@gmail.com>
2018-11-01 17:48:04 -0600

    I used the switch keyword in replace of the else if statement

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/The-switch-keyword.js
@@ -0,0 +1,17 @@
+let athleteFinalPosition = 'first place';
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log('You get the gold medal!')
+    break;
+  case 'second place':
+    console.log('You get the silver medal!')
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!')
+    break;
+  default:
+    console.log('No medal awarded.')
+    break
+}
+
+// I just learned about the switch keyword and how we can use it instead of else if statements
