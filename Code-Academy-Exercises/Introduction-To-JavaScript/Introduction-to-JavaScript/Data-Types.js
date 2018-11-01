b5a53d2792ac26ac0469bfd71a3a8c5a7f67db21
Samuel Slike <samcslike@gmail.com>
2018-11-01 16:06:21 -0600

    I added in different data types to the consol.log's

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Data-Types.js
@@ -0,0 +1,6 @@
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
+// I just learned the different data types that can be displayed with the console.log()
