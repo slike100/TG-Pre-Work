280e8103f1df750f102c5eaf840fed20885eec23
Samuel Slike <samcslike@gmail.com>
2018-11-01 17:07:20 -0600

    I added in an else statement to the if statement

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/if-else-Statements.js
@@ -0,0 +1,11 @@
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+} else {
+console.log('Time to wait for a sale.')};
+
+
+// I just learned about if else statements and how they are used
