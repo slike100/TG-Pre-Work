cb176996acba172e7d9d2cb501e14393e03f07c4
Samuel Slike <samcslike@gmail.com>
2018-11-01 17:27:49 -0600

    I used short-circuit evaluation to assign a value to a variable

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -0,0 +1,8 @@
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+// I just learned about truthy and falsy assignment and also short-circuit evalutaion 
