610aed2aa77e04bbdd5caba379c4437e9eda060d
Samuel Slike <samcslike@gmail.com>
2018-11-03 15:17:33 -0600

    Parameters and Arguments

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Parameters-and-Arguments.js
@@ -0,0 +1,7 @@
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+};
+
+sayThanks('Cole');
+
+// I just learned abot parameters and Arguments in functions
