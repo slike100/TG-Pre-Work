4992eec6cd47d8e7651d7e9ac07d71434c40f97d
Samuel Slike <samcslike@gmail.com>
2018-11-03 16:06:01 -0600

    Blocks and Scope

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Blocks-and-Scope.js
@@ -0,0 +1,10 @@
+const city = 'New York City';
+
+const logCitySkyline = () => {
+  let skyscraper = 'Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+};
+
+console.log(logCitySkyline());
+
+// I just learned about blocks and their use
