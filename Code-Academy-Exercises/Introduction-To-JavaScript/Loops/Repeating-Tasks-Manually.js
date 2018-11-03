8b18969e5f8833dfd0205c680c32ad3bdd293209
Samuel Slike <samcslike@gmail.com>
2018-11-03 17:44:59 -0600

    Repeating Tasks Manually

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,10 @@
+// Write your code below
+const vacationSpots = ['Laguna', 'New Zealand', 'Mars'];
+
+console.log(vacationSpots[0]);
+
+console.log(vacationSpots[1]);
+
+console.log(vacationSpots[2]);
+
+// I just repeated a task manually
