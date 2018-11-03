6be0b3ff22f6b31f6c21440fb7b1cfbc6b433d15
Samuel Slike <samcslike@gmail.com>
2018-11-03 16:47:45 -0600

    Array with let and const

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays-with-let-and-const.js
@@ -0,0 +1,15 @@
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+
+condiments = ['Mayo'];
+
+utensils[3] = 'Spoon';
+
+console.log(condiments);
+
+console.log(utensils);
+
+// I just learned about an array with let and const
