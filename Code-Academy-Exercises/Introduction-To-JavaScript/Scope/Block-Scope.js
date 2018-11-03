9777081dda6cf7bb6129d8d7224049922ac94f1b
Samuel Slike <samcslike@gmail.com>
2018-11-03 16:23:17 -0600

    Block Scope

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Block-Scope.js
@@ -0,0 +1,10 @@
+const logVisibleLightWaves = () => {
+  const lightWaves = 'Moonlight';
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
+
+console.log(lightWaves);
+
+// I just learned about local variables 
