51c0160873308f40bece74097effef48727ff312
Samuel Slike <samcslike@gmail.com>
2018-11-03 16:33:10 -0600

    Practice Good Scoping

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Practice-and-Good-Scoping.js
@@ -0,0 +1,15 @@
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  if (region === 'The Arctic') {
+    let lightWaves = 'Northern Lights'
+    console.log(lightWaves)
+  };
+  // Add if statement here:
+
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
+
+// I just learned how to practice good scoping
