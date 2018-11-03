23e046f7970a8b853972be6d542818ab0ae7b700
Samuel Slike <samcslike@gmail.com>
2018-11-03 16:53:10 -0600

    The .pop() Method

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-pop-method.js
@@ -0,0 +1,7 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+chores.pop();
+
+console.log(chores);
+
+// I just learned about the pop method for removing an item from an array
