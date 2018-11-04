ab6c4e35b0ada17eed45e1c2ce9458fa4f07c3ab
Samuel Slike <samcslike@gmail.com>
2018-11-04 09:54:00 -0700

    The .forEach()Method

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-forEach-Method.js
@@ -0,0 +1,8 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(function(fruits){
+  console.log('I want to eat a ' + fruits)
+});
+
+// I just learned about the for each method
