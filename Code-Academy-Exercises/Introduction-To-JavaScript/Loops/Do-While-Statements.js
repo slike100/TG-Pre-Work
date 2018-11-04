55e805c9072a4cfe2e2dbb8dfd5e325bc27eadd9
Samuel Slike <samcslike@gmail.com>
2018-11-03 18:35:50 -0600

    Do...While Statements

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Do-While-Statements.js
@@ -0,0 +1,9 @@
+// Write your code below
+const cupsOfSugarNeeded = (2);
+const cupsAdded = (0);
+
+do {
+  cupsAdded++
+  } while (cupsAdded < cupsOfSugarNeeded);
+
+// I just learned about do while statements
