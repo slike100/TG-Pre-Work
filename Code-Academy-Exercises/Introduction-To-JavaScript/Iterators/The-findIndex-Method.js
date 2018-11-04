e0ab8a47fba1bd61510b8c9393bef3768b324311
Samuel Slike <samcslike@gmail.com>
2018-11-04 10:16:22 -0700

    The .findIndex() Method

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-findIndex-Method.js
@@ -0,0 +1,12 @@
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(animal => {
+  return animal === 'elephant';
+});
+
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] === 's' ? true : false;
+});
+
+
+// I just learned about the find index method
