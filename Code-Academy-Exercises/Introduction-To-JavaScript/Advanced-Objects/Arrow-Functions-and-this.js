1da081fe9572b3aec5b5c41be8e8c0d79dc9c750
Samuel Slike <samcslike@gmail.com>
2018-11-05 16:07:09 -0700

    Arrow Functions and this

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Arrow-Functions-and-this.js
@@ -0,0 +1,10 @@
+const robot = {
+  energyLevel: 100,
+  checkEnergy(){
+    console.log(`Energy is currently at ${this.energyLevel}%.`)
+  }
+}
+
+robot.checkEnergy();
+
+// I just learned about using the arrow functions and this
