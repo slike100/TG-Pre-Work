42b589f61eeb0f603155cc9aae26010d43d939ef
Samuel Slike <samcslike@gmail.com>
2018-11-04 20:41:03 -0700

    Methods

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Methods.js
@@ -0,0 +1,18 @@
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
+
+alienShip.retreat();
+
+alienShip.takeOff();
+
+// I just learned about methods which we have been using all along
