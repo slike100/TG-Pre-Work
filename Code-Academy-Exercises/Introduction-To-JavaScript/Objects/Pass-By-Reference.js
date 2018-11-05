02b8dbd873a0de9336cb7bd643a29c505ac9cb70
Samuel Slike <samcslike@gmail.com>
2018-11-04 21:00:05 -0700

    Pass By Reference

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Pass-By-Reference.js
@@ -0,0 +1,22 @@
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+
+let greenEnergy = obj => {
+  obj['Fuel Type'] = 'avocado oil';
+}
+
+let remotelyDisable = obj => {
+  obj.disabled = true;
+}
+
+greenEnergy(spaceship);
+
+remotelyDisable(spaceship);
+
+console.log(spaceship)
+
+// I just learned about pass by reference
