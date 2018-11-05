77a6afb0c0876cde71a02bb1961f269b08f5ed95
Samuel Slike <samcslike@gmail.com>
2018-11-05 16:55:04 -0700

    caniuse.com II

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,7 @@
+// Set the variable below to a number
+let esFivePercentageSupport = 97;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 78;
+
+// I just learned to use caniuse.com
