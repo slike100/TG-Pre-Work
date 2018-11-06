2a1d716956c3e7cf8e68b16f5d6d4b7ca12ce20c
Samuel Slike <samcslike@gmail.com>
2018-11-05 17:19:32 -0700

    require()

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/require.js
@@ -0,0 +1,9 @@
+const Airplane = require('./1-airplane.js');
+
+function displayAirplane() {
+  console.log(Airplane.myAirplane);
+}
+
+displayAirplane();
+
+// I just learned to use require in order to import the module and use its properties
