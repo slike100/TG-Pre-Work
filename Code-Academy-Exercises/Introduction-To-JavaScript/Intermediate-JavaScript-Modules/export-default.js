14e22e8a9f166454a305f9669b1ea5891b72a31e
Samuel Slike <samcslike@gmail.com>
2018-11-05 17:28:59 -0700

    export default

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/export-default.js
@@ -0,0 +1,17 @@
+let Airplane = {};
+
+Airplane.availableAirplanes = [];
+
+Airplane.availableAirplanes = [
+{
+  name: 'AeroJet',
+  fuelCapacity: 800
+ },
+ {name: 'SkyJet',
+  fuelCapacity: 500
+ }
+];
+
+export default Airplane;
+
+// I just learned about export default 
