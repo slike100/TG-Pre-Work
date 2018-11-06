e8548002e5af36069a0b50b09c535f0b6dacc423
Samuel Slike <samcslike@gmail.com>
2018-11-05 17:38:50 -0700

    Named Exports

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Named-exports.js
@@ -0,0 +1,31 @@
+
+let availableAirplanes = [
+{
+  name: 'AeroJet',
+  fuelCapacity: 800,
+  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance','sensorOperators']
+ },
+ {name: 'SkyJet',
+  fuelCapacity: 500,
+  availableStaff: ['pilots','flightAttendants']
+ }
+];
+
+let flightRequirements = {
+  requiredStaff: 4,
+};
+
+function meetsStaffRequirements(availableStaff, requiredStaff) {
+};
+
+function meetsStaffRequirements(availableStaff, requiredStaff) {
+  if (availableStaff.length >= requiredStaff) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export { availableAirplanes, flightRequirements, meetsStaffRequirements};
+
+// I just learned about named exports
