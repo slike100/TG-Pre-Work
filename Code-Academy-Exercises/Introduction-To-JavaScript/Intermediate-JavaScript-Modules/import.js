15e2f0b99ca572510186dbdd30d41ba020361eea
Samuel Slike <samcslike@gmail.com>
2018-11-05 17:32:34 -0700

    import

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/import.js
@@ -0,0 +1,11 @@
+import Airplane from './airplane';
+
+function displayFuelCapacity() {
+  Airplane.availableAirplanes.forEach(function(element){
+  console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
+
+// I just learned about the import keyword for importing objects
