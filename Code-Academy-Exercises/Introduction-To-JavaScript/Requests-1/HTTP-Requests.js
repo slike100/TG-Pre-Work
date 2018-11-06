2b28be74dfd9971d4ca7f7128f4c215c74e9b2d5
Samuel Slike <samcslike@gmail.com>
2018-11-06 16:30:30 -0700

    HTTP Requests

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-1/HTTP-Requests.js
@@ -0,0 +1,8 @@
+console.log('First message!');
+setTimeout(() => {
+   console.log('This message will always run last...');
+}, 0);
+console.log('Second message!');
+
+
+// I just learned a little bit about asynchronous language in JS
