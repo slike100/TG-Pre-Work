416e7b0a7b23083aeb90da582e8e705a602cb11b
Samuel Slike <samcslike@gmail.com>
2018-11-06 17:28:09 -0700

    fetch() Post Requests III

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-POST-Requests-3.js
@@ -0,0 +1,37 @@
+// Information to reach API
+const apiKey = '8c98e5a05a4240fdaadfdf950d86a9da';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten})
+
+	fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  })
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+
+// I just learned to update the boilerplate code for making a post request
